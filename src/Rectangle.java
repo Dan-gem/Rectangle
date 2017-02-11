a82794f7a21fa603c67fd39ca8515c8472571a61
Dan-gem <Dan_dan_gen@mail.ru>
2017-02-11 18:00:19 +0600

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+
+public class Rectangle {
+    public static void main(String[] args) {
+        int a, b;
+        for (a = 0; a < 10; a++) {
+
+            for (b = 0; b < 30; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
